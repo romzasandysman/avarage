7e79b797b812ef6de7dba46646354f839973c45f
Alexander <romzasandysman@gmail.com>
2021-02-03 10:32:52 +0700

    save project

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/romzasandysman/datafile"
+	"log"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt")
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	var sum float64 = 0
+	for _, number := range numbers{
+		sum += number
+	}
+
+	simpleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/simpleCount)
+}
